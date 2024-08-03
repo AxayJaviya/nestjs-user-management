6dbd4a064a338ce92143ce99b2d30691021ebd95
AxayJaviya <javiyaaxay@gmail.com>
2024-08-03 22:15:38 +0200

    [UPDATE] Update in-memory token service to store blacklisted tokens in file

--- backend/src/modules/auth/repositories/in-memory.tokens.repository.spec.ts
+++ backend/src/modules/auth/repositories/in-memory.tokens.repository.spec.ts
@@ -1,10 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { InMemoryTokensRepository } from './in-memory.tokens.repository';
 
 describe('InMemoryTokensRepository', () => {
   let repository: InMemoryTokensRepository;
+  const filePath = path.resolve(__dirname, 'data', 'tokens.json');
+
+  // Helper function to clear the file
+  const clearFile = () => {
+    if (fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, JSON.stringify([]));
+    }
+  };
 
   beforeEach(() => {
     repository = new InMemoryTokensRepository();
+    clearFile();
+  });
+
+  afterEach(() => {
+    // To ensure, a file is cleared after each test
+    clearFile();
   });
 
   it('should be defined', () => {
--- backend/src/modules/auth/repositories/in-memory.tokens.repository.ts
+++ backend/src/modules/auth/repositories/in-memory.tokens.repository.ts
@@ -1,16 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import * as path from 'path';
+import { FileStorage } from '../../utils/fileStorage';
 import { TokensRepository } from '../interfaces/tokens-repository.interface';
 
 @Injectable()
 export class InMemoryTokensRepository implements TokensRepository {
-  private readonly tokens = new Map<string, Date>();
+  private readonly filePath: string;
+  private tokens: Map<string, Date>;
+
+  constructor() {
+    this.filePath = path.resolve(__dirname, 'data', 'tokens.json');
+    FileStorage.ensureDirectoryExistence(this.filePath);
+    this.tokens = this.loadTokens();
+  }
 
   async invalidateToken(token: string): Promise<void> {
     this.tokens.set(token, new Date());
+
+    this.saveTokens();
   }
 
   async isBlacklisted(token: string): Promise<boolean> {
-    const expiredToken = this.tokens.get(token);
-    return !!expiredToken;
+    this.tokens = this.loadTokens();
+
+    return this.tokens.has(token);
+  }
+
+  private loadTokens(): Map<string, Date> {
+    try {
+      const tokensData = FileStorage.loadFromFile<{
+        token: string;
+        date: string;
+      }>(this.filePath);
+      return new Map(
+        tokensData.map(({ token, date }) => [token, new Date(date)]),
+      );
+    } catch (error) {
+      throw new Error('Failed to load tokens!');
+    }
+  }
+
+  private saveTokens(): void {
+    try {
+      const tokensArray = Array.from(this.tokens.entries()).map(
+        ([token, date]) => ({
+          token,
+          date: date.toISOString(),
+        }),
+      );
+      FileStorage.saveToFile(this.filePath, tokensArray);
+    } catch (error) {
+      throw new Error('Failed to save tokens!');
+    }
   }
 }
