d61caddac1d4e8d8f68f89df4ca0cd37a165c820
AxayJaviya <javiyaaxay@gmail.com>
2024-08-04 16:23:09 +0200

    [ADD] Added basic swagger api documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,29 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import * as dotenv from 'dotenv';
-import * as process from 'node:process';
+import * as process from 'process';
 import { AppModule } from './app.module';
 
 // Load the appropriate .env file based on NODE_ENV
-dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+dotenv.config({ path: envFilePath });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS User Management APIs')
+    .setDescription(
+      'API documentation for managing user registration, authentication, and profile management',
+    )
+    .setVersion('1.0')
+    .addTag('users', 'Operations related to user management')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Apply validation pipe globally
   app.useGlobalPipes(
@@ -20,6 +34,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
 }
+
 bootstrap();
